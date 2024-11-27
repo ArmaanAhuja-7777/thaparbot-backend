cdb83e89be1580e5bc0084749153f168c3b6584d
ArmaanAhuja-7777 <ahujaarmaan7777@gmail.com>
2024-11-27 12:09:22 +0530

    windows is fucked

--- routes/chatbotRoutes.js
+++ routes/chatbotRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {generateResponse} = require('../controllers/chatBotController');
+    const {generateResponse} = require('../controllers/chatbotController');
 
 const router = express.Router();
 
